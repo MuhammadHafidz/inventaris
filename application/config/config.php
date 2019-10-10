6b0e9b3f15ea692e1424bc3a5d11a8d8b5cc4e05
ilvinur <ilvinur8@gmail.com>
2019-10-10 20:56:34 +0700

    logo

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pem-framework/inventarisasi/';
+$config['base_url'] = 'http://localhost/Newfolder(3)/inventaris';
 
 /*
 |--------------------------------------------------------------------------
